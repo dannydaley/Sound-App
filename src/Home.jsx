02305c23a4d78f124eadda47c4a7e269e7571b9e
Warwick <warwick.l.e.new@gmail.com>
2023-05-29 20:39:45 +0100

    Added dound to the sound app. Now we have a platform we can run with.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as Tone from 'tone'
+
+
+const playsound = () => {
+    const synth = new Tone.PolySynth().toDestination();
+    // set the attributes across all the voices using 'set'
+    synth.set({ detune: -1200 });
+    // play a chord
+    synth.triggerAttackRelease(["C5", "E5", "A5"], 1);
+}
 
 const Home = () => {
-    return <div>Sound Application</div>
+    return <>
+        <div>Sound Application</div>
+        <button onClick={playsound}>Press for sound</button>
+    </>
 }
 
 export default Home
