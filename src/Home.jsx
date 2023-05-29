7832385dbbd83e57d61802b835eb0cfb0db18fcb
Warwick <warwick.l.e.new@gmail.com>
2023-05-29 20:17:57 +0100

    Added React into the build system.

---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+    return <div>Sound Application</div>
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import Home from './Home';
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(<Home />);
