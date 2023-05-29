28cea1c475e1911c25362d15de5440da91d9f2c7
Danny Daley <dd252935@falmouth.ac.uk>
2023-05-29 22:41:50 +0100

    Added 3 tones that can be triggered by the a, c and e keys if input field is focused

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,63 @@
-import React, { useEffect } from 'react';
-import * as Tone from 'tone'
+import React, { useEffect } from "react";
+import * as Tone from "tone";
 
+// const playsound = () => {
+//     const synth = new Tone.PolySynth().toDestination();
+//     // set the attributes across all the voices using 'set'
+//     synth.set({ detune: -1200 });
+//     // play a chord
+//     synth.triggerAttackRelease(["C5", "E5", "A5"], 1);
+// }
 
-const playsound = () => {
+const playsound = (event) => {
     const synth = new Tone.PolySynth().toDestination();
     // set the attributes across all the voices using 'set'
     synth.set({ detune: -1200 });
     // play a chord
-    synth.triggerAttackRelease(["C5", "E5", "A5"], 1);
-}
+    console.log(event.key);
+    switch (event.key.toUpperCase()) {
+        case "C":
+            synth.triggerAttackRelease(["C5"], 1);
 
+            // code block
+            break;
+        case "E":
+            synth.triggerAttackRelease(["E5"], 1);
+            // code block
+            break;
+        case "A":
+            synth.triggerAttackRelease(["A5"], 1);
+            // code block
+            break;
+        default:
+            synth.triggerAttackRelease(["C5", "E5", "A5"], 1);
+    }
+};
+// document.onKeyDown(playsound());
 const Home = () => {
-    return <>
-        <div>Sound Application</div>
-        <button onClick={playsound}>Press for sound</button>
-    </>
-}
+    return (
+        <>
+            <div
+                style={{
+                    width: "100%",
+                    height: "500px",
+                    backgroundColor: "gray",
+                }}
+            >
+                <input
+                    autoFocus="true"
+                    type="text"
+                    onKeyDown={(event) => {
+                        event.preventDefault();
+                        playsound(event);
+                    }}
+                ></input>
 
-export default Home
+                <div>Sound Application</div>
+                <button onClick={playsound}>Press for sound</button>
+            </div>
+        </>
+    );
+};
+
+export default Home;
