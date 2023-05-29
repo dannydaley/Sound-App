5dd3a779e86d36caef9f69438bb13411c7683726
Danny Daley <dd252935@falmouth.ac.uk>
2023-05-29 22:52:46 +0100

    Comment, clean up & maintainability

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,29 +11,30 @@ import * as Tone from "tone";
 
 const playsound = (event) => {
     const synth = new Tone.PolySynth().toDestination();
+
     // set the attributes across all the voices using 'set'
     synth.set({ detune: -1200 });
-    // play a chord
+
+    // console log (key for reference)
     console.log(event.key);
+
+    // play note depending on key press
+
     switch (event.key.toUpperCase()) {
         case "C":
             synth.triggerAttackRelease(["C5"], 1);
-
-            // code block
             break;
         case "E":
             synth.triggerAttackRelease(["E5"], 1);
-            // code block
             break;
         case "A":
             synth.triggerAttackRelease(["A5"], 1);
-            // code block
             break;
         default:
             synth.triggerAttackRelease(["C5", "E5", "A5"], 1);
     }
 };
-// document.onKeyDown(playsound());
+
 const Home = () => {
     return (
         <>
@@ -51,8 +52,7 @@ const Home = () => {
                         event.preventDefault();
                         playsound(event);
                     }}
-                ></input>
-
+                />
                 <div>Sound Application</div>
                 <button onClick={playsound}>Press for sound</button>
             </div>
