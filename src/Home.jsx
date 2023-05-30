5eea0c3d66c4c014adf70bae9f4bceb93dcd8d62
Danny Daley <dd252935@falmouth.ac.uk>
2023-05-30 14:11:50 +0100

    Added more notes to the onscreen keyboard giving us a full single octave. Different keys have been mapped to the notes to give a more natural and intuitive experience of the notes so can be played quite closely to the real thing. Sharp notes (ie C#) have also been added to the onscreen keyboard.

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,17 +11,70 @@ import * as Tone from "tone";
 
 // function that changes the background colour of the keys (divs) to represent a press
 const keyEffect = (note) => {
+    // set up empty tone variable to switch from key input to note played
+    let tone;
+
+    // the switch..
+    switch (note) {
+        case "A":
+            tone = "C";
+            break;
+        case "W":
+            tone = "C-Sharp";
+            break;
+        case "S":
+            tone = "D";
+            break;
+        case "E":
+            tone = "D-Sharp";
+            break;
+        case "D":
+            tone = "E";
+            break;
+        case "F":
+            tone = "F";
+            break;
+        case "T":
+            tone = "F-Sharp";
+            break;
+        case "G":
+            tone = "G";
+            break;
+        case "Y":
+            tone = "G-Sharp";
+            break;
+        case "H":
+            tone = "A";
+            break;
+        case "U":
+            tone = "A-Sharp";
+            break;
+        case "J":
+            tone = "B";
+            break;
+        case "A":
+            tone = "C";
+            break;
+        case "A":
+            tone = "C";
+            break;
+    }
+    // if a dom element with the tone as the ID exists, change the dom element colour
     if (
-        document.getElementById(note + "-Key") !== null &&
-        document.getElementById(note + "-Key") !== undefined
+        document.getElementById(tone + "-Key") !== null &&
+        document.getElementById(tone + "-Key") !== undefined
     ) {
+        // make note of what the original coloue was..
+        let noteColour = document.getElementById(tone + "-Key").style
+            .backgroundColor;
+
         // change key to grey
-        document.getElementById(note + "-Key").style.backgroundColor = "grey";
+        document.getElementById(tone + "-Key").style.backgroundColor = "grey";
 
         // wait 500 miliseconds then change to white
         setTimeout(() => {
-            document.getElementById(note + "-Key").style.backgroundColor =
-                "white";
+            document.getElementById(tone + "-Key").style.backgroundColor =
+                noteColour;
         }, 500);
     }
 };
@@ -39,25 +92,40 @@ const playsound = (event) => {
     if (event.key) {
         // play note depending on key press
         switch (event.key.toUpperCase()) {
-            case "C":
+            case "A":
                 synth.triggerAttackRelease(["C5"], 1);
                 break;
-            case "D":
+            case "W":
+                synth.triggerAttackRelease(["C#5"], 1);
+                break;
+            case "S":
                 synth.triggerAttackRelease(["D5"], 1);
                 break;
             case "E":
+                synth.triggerAttackRelease(["D#5"], 1);
+                break;
+            case "D":
                 synth.triggerAttackRelease(["E5"], 1);
                 break;
             case "F":
                 synth.triggerAttackRelease(["F5"], 1);
                 break;
+            case "T":
+                synth.triggerAttackRelease(["F#5"], 1);
+                break;
             case "G":
                 synth.triggerAttackRelease(["G5"], 1);
                 break;
-            case "A":
+            case "Y":
+                synth.triggerAttackRelease(["G#5"], 1);
+                break;
+            case "H":
                 synth.triggerAttackRelease(["A5"], 1);
                 break;
-            case "B":
+            case "U":
+                synth.triggerAttackRelease(["A#5"], 1);
+                break;
+            case "J":
                 synth.triggerAttackRelease(["B5"], 1);
                 break;
             default:
@@ -77,7 +145,6 @@ const Home = () => {
                 style={{
                     width: "100%",
                     height: "500px",
-                    // backgroundColor: "gray",
                 }}
             >
                 <input
@@ -93,78 +160,234 @@ const Home = () => {
 
                 <div
                     id="keyboard"
-                    style={{ display: "flex", margin: "50px 100px" }}
+                    style={{
+                        border: "2px solid black",
+                        width: "500px",
+                        margin: "100px 100px",
+                    }}
                 >
                     <div
-                        id="C-Key"
+                        id="Major-keys"
                         style={{
-                            width: "50px",
-                            height: "175px",
-                            backgroundColor: "white",
+                            width: "380px",
+                            zIndex: "1",
+                            zIndex: 9,
                             border: "2px solid black",
-                            borderRadius: "2px",
+                            display: "flex",
                         }}
-                    ></div>
+                    >
+                        <div
+                            id="C-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "black",
+                                width: "50px",
+                                height: "175px",
+                                backgroundColor: "white",
+                                border: "2px solid black",
+                                borderRadius: "2px",
+                            }}
+                        >
+                            {"A"}
+                        </div>
+                        <div
+                            id="D-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "black",
+                                width: "50px",
+                                height: "175px",
+                                backgroundColor: "white",
+                                border: "2px solid black",
+                                borderRadius: "2px",
+                            }}
+                        >
+                            {"S"}
+                        </div>
+                        <div
+                            id="E-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "black",
+                                width: "50px",
+                                height: "175px",
+                                backgroundColor: "white",
+                                border: "2px solid black",
+                                borderRadius: "2px",
+                            }}
+                        >
+                            {"D"}
+                        </div>
+                        <div
+                            id="F-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "black",
+                                width: "50px",
+                                height: "175px",
+                                backgroundColor: "white",
+                                border: "2px solid black",
+                                borderRadius: "2px",
+                            }}
+                        >
+                            {"F"}
+                        </div>
+                        <div
+                            id="G-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "black",
+                                width: "50px",
+                                height: "175px",
+                                backgroundColor: "white",
+                                border: "2px solid black",
+                                borderRadius: "2px",
+                            }}
+                        >
+                            {"G"}
+                        </div>
+                        <div
+                            id="A-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "black",
+                                width: "50px",
+                                height: "175px",
+                                backgroundColor: "white",
+                                border: "2px solid black",
+                                borderRadius: "2px",
+                            }}
+                        >
+                            {"H"}
+                        </div>
+                        <div
+                            id="B-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "black",
+                                width: "50px",
+                                height: "175px",
+                                backgroundColor: "white",
+                                border: "2px solid black",
+                                borderRadius: "2px",
+                            }}
+                        >
+                            {"J"}
+                        </div>
+                    </div>
                     <div
-                        id="D-Key"
+                        id="Sharp-Keys"
                         style={{
-                            width: "50px",
-                            height: "175px",
-                            backgroundColor: "white",
                             border: "2px solid black",
-                            borderRadius: "2px",
+                            display: "flex",
+                            flexDirection: "row",
+                            width: "350px",
+                            zIndex: "0",
+                            zIndex: 3,
+                            marginTop: "-185px",
                         }}
-                    ></div>
+                    >
+                        <div
+                            id="C-Sharp-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "white",
+                                width: "35px",
+                                height: "150px",
+                                backgroundColor: "black",
+                                borderRadius: "3px",
+                                marginLeft: "35px",
+                            }}
+                        >
+                            {"W"}
+                        </div>
+                        <div
+                            id="D-Sharp-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "white",
+                                width: "35px",
+                                height: "150px",
+                                backgroundColor: "black",
+                                borderRadius: "3px",
+                                marginLeft: "20px",
+                            }}
+                        >
+                            {"E"}
+                        </div>
+                        <div
+                            id="F-Sharp-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "white",
+                                width: "35px",
+                                height: "150px",
+                                backgroundColor: "black",
+                                borderRadius: "3px",
+                                marginLeft: "70px",
+                            }}
+                        >
+                            {"T"}
+                        </div>
+                        <div
+                            id="G-Sharp-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "white",
+                                width: "35px",
+                                height: "150px",
+                                backgroundColor: "black",
+                                borderRadius: "3px",
+                                marginLeft: "20px",
+                            }}
+                        >
+                            {"Y"}
+                        </div>
+                        <div
+                            id="A-Sharp-Key"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                color: "white",
+                                width: "35px",
+                                height: "150px",
+                                backgroundColor: "black",
+                                borderRadius: "3px",
+                                marginLeft: "20px",
+                            }}
+                        >
+                            {"U"}
+                        </div>
+                    </div>
                     <div
-                        id="E-Key"
                         style={{
-                            width: "50px",
-                            height: "175px",
-                            backgroundColor: "white",
+                            width: "110px",
+                            marginLeft: "auto",
+                            marginTop: "-150px",
                             border: "2px solid black",
-                            borderRadius: "2px",
-                        }}
-                    ></div>
-                    <div
-                        id="F-Key"
-                        style={{
-                            width: "50px",
                             height: "175px",
-                            backgroundColor: "white",
-                            border: "2px solid black",
-                            borderRadius: "2px",
+                            alignSelf: "right",
+                            display: "flex",
+                            flexDirection: "column",
+                            backgroundColor: "black",
                         }}
-                    ></div>
-                    <div
-                        id="G-Key"
-                        style={{
-                            width: "50px",
-                            height: "175px",
-                            backgroundColor: "white",
-                            border: "2px solid black",
-                            borderRadius: "2px",
-                        }}
-                    ></div>
-                    <div
-                        id="A-Key"
-                        style={{
-                            width: "50px",
-                            height: "175px",
-                            backgroundColor: "white",
-                            border: "2px solid black",
-                            borderRadius: "2px",
-                        }}
-                    ></div>
-                    <div
-                        id="B-Key"
-                        style={{
-                            width: "50px",
-                            height: "175px",
-                            backgroundColor: "white",
-                            border: "2px solid black",
-                            borderRadius: "2px",
-                        }}
-                    ></div>
+                    >
+                        <input type="range"></input>
+                        <input type="range"></input>
+                        <input type="range"></input>
+                        <input type="radio"></input>
+                    </div>
                 </div>
             </div>
         </>
